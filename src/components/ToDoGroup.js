f084d136fefe27260835b3bb644be30e051d24f1
Gerard Kamran Kalambacal <kamrangerardkalambacal@gmail.com>
2023-08-28 19:43:03 +0800

    refactor: removed double h1 inside ToDoGroup

--- src/components/ToDoGroup.js
+++ src/components/ToDoGroup.js
@@ -4,7 +4,6 @@ import ToDoItem from './ToDoItem';
 const ToDoGroup = ({ items }) => {
   return (
     <div>
-      <h1>To Do List</h1>
       <ul>
         {items.map((item, index) => (
           <ToDoItem key={index} text={item} />
